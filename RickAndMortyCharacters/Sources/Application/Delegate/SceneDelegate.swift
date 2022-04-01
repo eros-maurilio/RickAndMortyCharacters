da90f1ed8bf04c181fa89ce49a1242246cd477ce
Eros Maurilio <eros.maurilio@gmail.com>
2022-04-01 14:15:24 -0300

    Remove storyboard and create a view programatically

--- RickAndMortyCharacters/Sources/Application/Delegate/SceneDelegate.swift
+++ RickAndMortyCharacters/Sources/Application/Delegate/SceneDelegate.swift
@@ -7,7 +7,16 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
 
-        guard let _ = (scene as? UIWindowScene) else { return }
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        
+        let window = UIWindow(windowScene: windowScene)
+        let viewController = ViewController()
+        let navigationController = UINavigationController(rootViewController: viewController)
+        
+        window.rootViewController = navigationController
+        
+        self.window = window
+        window.makeKeyAndVisible()
     }
 
     func sceneDidDisconnect(_ scene: UIScene) { }
--- RickAndMortyCharacters/Sources/Application/ViewController.swift
+++ RickAndMortyCharacters/Sources/Application/ViewController.swift
@@ -5,5 +5,6 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        view.backgroundColor = .blue
     }
 }

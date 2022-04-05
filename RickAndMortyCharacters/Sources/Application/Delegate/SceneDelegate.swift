fa5d593e7502e1a142d7b6020ca853efd78a056f
Eros Maurilio <eros.maurilio@gmail.com>
2022-04-05 16:05:00 -0300

    Set first ViewController

--- RickAndMortyCharacters/Sources/Application/Delegate/SceneDelegate.swift
+++ RickAndMortyCharacters/Sources/Application/Delegate/SceneDelegate.swift
@@ -10,7 +10,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         
         let window = UIWindow(windowScene: windowScene)
-        let viewController = ViewController()
+        let viewController = CharactersListViewController()
         let navigationController = UINavigationController(rootViewController: viewController)
         
         window.rootViewController = navigationController

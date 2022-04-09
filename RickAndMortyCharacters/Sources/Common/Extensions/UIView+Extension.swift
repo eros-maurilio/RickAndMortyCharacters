6518a5e17be21dc25cb235e64bca5cd5ec97acb4
Eros Maurilio <eros.maurilio@gmail.com>
2022-04-09 11:25:57 -0300

    Create shadow and corner radius methods

--- RickAndMortyCharacters/Sources/Common/Extensions/UIView+Extension.swift
+++ RickAndMortyCharacters/Sources/Common/Extensions/UIView+Extension.swift
@@ -5,6 +5,17 @@ extension UIView {
         translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate(constraints(self))
     }
+    
+    func applyCornerRadius(_ cornerRadius: CGFloat) {
+        layer.cornerRadius = cornerRadius
+    }
+    
+    func applyShadow() {
+        layer.shadowColor = UIColor.black.cgColor
+        layer.shadowOpacity = 0.25
+        layer.shadowOffset = CGSize(width: 2, height: 4)
+        layer.shadowRadius = 3
+    }
 }
 
 @resultBuilder

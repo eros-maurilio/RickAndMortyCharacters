e441be783a83919e5d8ad0afb7610e483315f1fe
Eros Maurilio <eros.maurilio@gmail.com>
2022-04-09 09:27:39 -0300

    Add custom UIColor extension

---
+++ RickAndMortyCharacters/Sources/Common/Extensions/UIColor+Extension.swift
@@ -0,0 +1,9 @@
+import UIKit
+
+extension UIColor {
+    static let neonGreen = UIColor(named: "NeonGreen")
+    static let paleGreen = UIColor(named: "PaleGreen")
+    static let lightGreen = UIColor(named: "LightGreen")
+    static let lightGray = UIColor(named: "LightGray")
+    static let deepGreen = UIColor(named: "DeepGreen")
+}

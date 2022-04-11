d4fa5a44f8c07d522a5d971764607b232afc3bab
Eros Maurilio <eros.maurilio@gmail.com>
2022-04-11 14:47:25 -0300

    Change Cell method to conform Character model

--- RickAndMortyCharacters/Sources/Application/CharacterListAdapting.swift
+++ RickAndMortyCharacters/Sources/Application/CharacterListAdapting.swift
@@ -17,10 +17,10 @@ final class CharacterTableViewAdapter: NSObject, CharacterListAdapting {
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        cell(tableView, at: indexPath, forACellDTO: viewModel[indexPath.row].name)
+        cell(tableView, at: indexPath, forACellDTO: viewModel[indexPath.row])
     }
     
-    private func cell(_ tableView: UITableView, at indexPath: IndexPath, forACellDTO ACellDTO: String) -> CharacterCell {
+    private func cell(_ tableView: UITableView, at indexPath: IndexPath, forACellDTO ACellDTO: Character) -> CharacterCell {
         let cell = tableView.dequeueCell(CharacterCell.self, indexPath)
         cell.fill(dto: ACellDTO)
         return cell

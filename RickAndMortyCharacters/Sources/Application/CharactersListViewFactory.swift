81e669dd13e10b08c5bc74ca5af1a0adea11db01
Eros Maurilio <eros.maurilio@gmail.com>
2022-04-20 15:17:05 -0300

    Add delegate injection

--- RickAndMortyCharacters/Sources/Application/CharactersListViewFactory.swift
+++ RickAndMortyCharacters/Sources/Application/CharactersListViewFactory.swift
@@ -4,13 +4,14 @@ enum CharactersListViewFactory {
     static func create() -> UIViewController {
         
         let router = CharactersListRouter()
-        let adapter = CharacterTableViewAdapter()
         let interactor = CharactersListInteractor()
+        let adapter = CharacterTableViewAdapter()
         let viewController = CharactersListViewController(interactor: interactor,
                                                           adapter: adapter,
                                                           router: router)
         let presenter = CharactersListPresenter()
         interactor.presenter = presenter
+        adapter.injectDelegate(viewController)
         presenter.viewController = viewController
         router.viewController = viewController
         return viewController

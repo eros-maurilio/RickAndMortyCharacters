592796a13026519af7d30637dc55a8b534476f61
Eros Maurilio <eros.maurilio@gmail.com>
2022-10-22 16:44:43 -0300

    Create aspectRatio method

--- RickAndMortyCharacters/Sources/Common/Extensions/UIView+Extension.swift
+++ RickAndMortyCharacters/Sources/Common/Extensions/UIView+Extension.swift
@@ -16,6 +16,10 @@ extension UIView {
         layer.shadowOffset = CGSize(width: 2, height: 4)
         layer.shadowRadius = 3
     }
+    
+    func viewAspectRatio(_ ratio: CGFloat) -> NSLayoutConstraint {
+    NSLayoutConstraint(item: self, attribute: .height, relatedBy: .equal, toItem: self, attribute: .width, multiplier: ratio, constant: 0)
+    }
 }
 
 @resultBuilder

34c2191cfc6b76355d0b447283f9039a0bf66714
Eros Maurilio <eros.maurilio@gmail.com>
2022-10-28 22:54:05 -0300

    Handle local store and requests

--- RickAndMortyCharacters/Sources/Application/CharactersListWorker.swift
+++ RickAndMortyCharacters/Sources/Application/CharactersListWorker.swift
@@ -1,34 +1,53 @@
 import Foundation
-
-struct CharacterDTO: Decodable {
-    let name: String
-    let image: String
-    let id: Int
-}
-
-struct Info: Decodable {
-    let pages: Int
-}
-
-struct CharactersListResponseDTO: Decodable {
-    let results: [CharacterDTO]
-    let info: Info
-}
+import CoreData
 
 final class CharactersListWorker {
-    typealias ListResult = Result<CharactersListResponseDTO, NSError>
     
+    typealias ListResult = Result<CharactersListResponseDTO, NSError>
+    typealias CoreDataResult = Result<[CharacterData], CacheError>
+
     func execute(withQuery query: String, completion: @escaping (CharactersListResponseDTO) -> Void) {
-        let dataLoaderManager = DataLoaderManager()
+        let dataLoaderManager = NetworkDataLoaderManager()
+        let localDataLoaderManager = LocalDataLoaderManager()
+        let endpoint: EndPoint = EndPoint.requestURLWith(.page, query: query)
         
-        dataLoaderManager.execute(.requestURLWith(.page, query: query)) { (result: ListResult) in
+        localDataLoaderManager.execute(endpoint) { [endpoint] (result: CoreDataResult) in
             switch result {
             case let .success(data):
-                completion(data)
-            case let .failure(error):
-                debugPrint(error)
-                break // TODO completion error
+                var characterList = [CharacterDTO]()
+                
+                data.forEach {
+                    let character = CharacterDTO(id: Int($0.characterId ?? "0") ?? 0,
+                                                 name: $0.name  ?? "",
+                                                 status: $0.status ?? "",
+                                                 species: $0.species ?? "",
+                                                 type: $0.type  ?? "",
+                                                 gender: $0.gender ?? "",
+                                                 origin: Origin(name: $0.origin?.name ?? ""),
+                                                 location: Location(name: $0.origin?.name ?? ""),
+                                                 image: $0.image ?? "",
+                                                 created: $0.created ?? "",
+                                                 endpoint: endpoint.url?.absoluteString ?? "")
+                    
+                    characterList.append(character)
+                }
+                
+                completion(CharactersListResponseDTO(results: characterList, info: InfoDTO(pages: 42)))
+                
+            case .failure(CacheError.miss):
+                dataLoaderManager.execute(.requestURLWith(.page, query: query)) { (result: ListResult) in
+                    switch result {
+                    case let .success(data):
+                        completion(data)
+                            localDataLoaderManager.saveInCoreDataWith(object: data.results)
+                        print(data)
+                    case let .failure(error):
+                        debugPrint(error)
+                    }
+                }
+                case .failure(.url):
+                    debugPrint("Bad Url")
+                }
             }
         }
     }
-}

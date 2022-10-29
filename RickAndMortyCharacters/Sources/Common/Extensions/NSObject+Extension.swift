af209474774b3a00c5ca022cdffd19f5d3e221e5
Eros Maurilio <eros.maurilio@gmail.com>
2022-10-28 22:37:35 -0300

    Create nsobject extension

---
+++ RickAndMortyCharacters/Sources/Common/Extensions/NSObject+Extension.swift
@@ -0,0 +1,9 @@
+import Foundation
+
+extension NSObject {
+    @objc
+    func value(forUndefinedKey key: String) -> Any {
+        NSError(domain: "Dynamic", code: 404, userInfo: [NSLocalizedDescriptionKey: "Accessing undefined key: '\(key)'"])
+    }
+}
+

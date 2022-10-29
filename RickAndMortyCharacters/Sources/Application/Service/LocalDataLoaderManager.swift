f7492eb8c06db8eba9cd4e5e2014a3adb5f41eb7
Eros Maurilio <eros.maurilio@gmail.com>
2022-10-29 13:23:22 -0300

    Create new fetch using id from core data

--- RickAndMortyCharacters/Sources/Application/Service/LocalDataLoaderManager.swift
+++ RickAndMortyCharacters/Sources/Application/Service/LocalDataLoaderManager.swift
@@ -28,29 +28,41 @@ final class LocalDataLoaderManager {
         completion(.success(fromDataBase))
     }
     
+    func executeWithId<T: NSManagedObject>(_ id: String, completion: @escaping (Result<[T], CacheError>) -> Void) {
+        let request = NSFetchRequest<NSFetchRequestResult>(entityName: String(describing: T.self))
+        request.fetchLimit = 1
+        request.predicate = .init(format: "characterId = %@", id)
+        
+        guard let fromDataBase = try? CoreDataStack.sharedInstance.persistentContainer.viewContext.fetch(request) as? [T],
+        !fromDataBase.isEmpty else { return completion(.failure(.miss)) }
+        
+        completion(.success(fromDataBase))
+    }
+    
     @discardableResult
-    func createAndInsertEntityFrom(character: CharacterDTO) -> NSManagedObject? {
+    func createAndInsertEntityFrom(character: CharacterDTO, endpoint: String) -> NSManagedObject? {
         let context = CoreDataStack.sharedInstance.persistentContainer.viewContext
         if let characterEntity = NSEntityDescription.insertNewObject(forEntityName: String(describing: CharacterData.self), into: context) as? CharacterData {
             
             characterEntity.characterId = String(character.id)
             characterEntity.name = character.name
             characterEntity.status = character.status
+            characterEntity.specie = character.species
             characterEntity.type = character.type
             characterEntity.gender = character.gender
-            characterEntity.origin?.name = character.origin?.name
-            characterEntity.location?.name = character.location?.name
+            characterEntity.originName = character.origin?.name
+            characterEntity.locationName = character.location?.name
             characterEntity.image = character.image
             characterEntity.created = character.created
-            characterEntity.endpoint = character.endpoint
+            characterEntity.endpoint = endpoint
             
             return characterEntity
         }
         return nil
     }
     
-    func saveInCoreDataWith(object: [CharacterDTO]) {
-        object.forEach { self.createAndInsertEntityFrom(character: $0) }
+    func saveInCoreDataWith(object: [CharacterDTO], endpoint: String) {
+        object.forEach { self.createAndInsertEntityFrom(character: $0, endpoint: endpoint) }
         
         do {
             try CoreDataStack.sharedInstance.persistentContainer.viewContext.save()
